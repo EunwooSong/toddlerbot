ca26704198986b4af9709cb1384a9f9415752c5d
EunwooSong <song200348@gmail.com>
2026-01-28 16:20:07 +0900

    fix: add

---
+++ rl/last_dance_3.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+
+# Run RL experiments sequentially
+# Define the different configurations for each experiment
+export MUJOCO_GL="egl"
+export XLA_PYTHON_CLIENT_MEM_FRACTION=".20"
+#export JAX_TRACEBACK_FILTERING="off"
+unset LD_LIBRARY_PATH
+
+robots=("toddlerbot")
+envs=("_T_Walk")
+#restore=("./results/toddlerbot__T_Walk_ppo_PPOConfig.num_timesteps=300000000,PPOConfig.num_evals=1000,PPOConfig.seed=0_20251120_195130/101068800")
+
+# 이 실험 스크립트는 리워드 함수를 제외하고, 환경적인 제약이 추가되었을 때의 성능을 평가하기 위한 실험입니다.
+# 따라서 다음과 같은 학습을 진행합니다.
+# 조절할 수 있는 환경적 제약 조건들: 
+# TJXEnvConfig.use_basic_obs, TJXEnvConfig.use_derate, TJXEnvConfig.use_group_rand
+# 따라서, 3!2 = 6가지의 조합에 대해 실험을 진행합니다.
+
+# config_overrides=(
+#     # 우리꺼 관측값은 그대로인거랑 관측값도 랜덤한거
+#     "PPOConfig.num_timesteps=500000000,PPOConfig.num_evals=10,PPOConfig.seed=0,TJXEnvConfig.use_basic_obs=False,TJXEnvConfig.threshold_ratio=0.3,HeatRewardScales.safety_penalty=0.0"
+#     "PPOConfig.num_timesteps=500000000,PPOConfig.num_evals=10,PPOConfig.seed=0,TJXEnvConfig.use_basic_obs=False,TJXEnvConfig.threshold_ratio=0.3"
+#     "PPOConfig.num_timesteps=500000000,PPOConfig.num_evals=10,PPOConfig.seed=0,TJXEnvConfig.use_basic_obs=False,TJXEnvConfig.threshold_ratio=0.5"
+#     "PPOConfig.num_timesteps=500000000,PPOConfig.num_evals=10,PPOConfig.seed=0,TJXEnvConfig.use_basic_obs=False,TJXEnvConfig.threshold_ratio=0.7"
+#     "PPOConfig.num_timesteps=500000000,PPOConfig.num_evals=10,PPOConfig.seed=0,TJXEnvConfig.use_basic_obs=False,TJXEnvConfig.threshold_ratio=0.9"
+#     "PPOConfig.num_timesteps=500000000,PPOConfig.num_evals=10,PPOConfig.seed=0,TJXEnvConfig.use_basic_obs=True,TJXEnvConfig.threshold_ratio=0.3"
+#     "PPOConfig.num_timesteps=500000000,PPOConfig.num_evals=10,PPOConfig.seed=0,TJXEnvConfig.use_basic_obs=True,TJXEnvConfig.use_derate=False,HeatRewardScales.safety_penalty=0.0"
+#     "PPOConfig.num_timesteps=500000000,PPOConfig.num_evals=10,PPOConfig.seed=0,TJXEnvConfig.use_basic_obs=True,TJXEnvConfig.use_derate=False,HeatRewardScales.safety_penalty=0.0,TJXEnvConfig.use_hard_const=True,TJXEnvConfig.torque=0.3"
+#     "PPOConfig.num_timesteps=500000000,PPOConfig.num_evals=10,PPOConfig.seed=0,TJXEnvConfig.use_basic_obs=True,TJXEnvConfig.use_derate=False,HeatRewardScales.safety_penalty=0.0,TJXEnvConfig.use_hard_const=True,TJXEnvConfig.torque=0.7"
+# )
+
+config_overrides=(
+    # 우리꺼 관측값은 그대로인거랑 관측값도 랜덤한거
+    #"PPOConfig.num_timesteps=500000000,PPOConfig.num_evals=10,PPOConfig.seed=0,TJXEnvConfig.use_basic_obs=False,TJXEnvConfig.threshold_ratio=0.3,HeatRewardScales.safety_penalty=0.0"
+    #"PPOConfig.num_timesteps=500000000,PPOConfig.num_evals=10,PPOConfig.seed=0,TJXEnvConfig.use_basic_obs=False,TJXEnvConfig.threshold_ratio=0.3"
+    "PPOConfig.num_timesteps=500000000,PPOConfig.num_evals=10,PPOConfig.seed=0,TJXEnvConfig.use_basic_obs=True,TJXEnvConfig.use_derate=False,HeatRewardScales.safety_penalty=0.0"
+    #"PPOConfig.num_timesteps=500000000,PPOConfig.num_evals=10,PPOConfig.seed=0,TJXEnvConfig.use_basic_obs=False,TJXEnvConfig.threshold_ratio=0.5"
+    #"PPOConfig.num_timesteps=500000000,PPOConfig.num_evals=10,PPOConfig.seed=0,TJXEnvConfig.use_basic_obs=False,TJXEnvConfig.threshold_ratio=0.7"
+    #"PPOConfig.num_timesteps=500000000,PPOConfig.num_evals=10,PPOConfig.seed=0,TJXEnvConfig.use_basic_obs=False,TJXEnvConfig.threshold_ratio=0.9"
+    #"PPOConfig.num_timesteps=500000000,PPOConfig.num_evals=10,PPOConfig.seed=0,TJXEnvConfig.use_basic_obs=True,TJXEnvConfig.threshold_ratio=0.3"
+    "PPOConfig.num_timesteps=500000000,PPOConfig.num_evals=10,PPOConfig.seed=0,TJXEnvConfig.use_basic_obs=True,TJXEnvConfig.use_derate=False,HeatRewardScales.safety_penalty=0.0,TJXEnvConfig.use_hard_const=True,TJXEnvConfig.torque=0.3"
+    #"PPOConfig.num_timesteps=500000000,PPOConfig.num_evals=10,PPOConfig.seed=0,TJXEnvConfig.use_basic_obs=True,TJXEnvConfig.use_derate=False,HeatRewardScales.safety_penalty=0.0,TJXEnvConfig.use_hard_const=True,TJXEnvConfig.torque=0.7"
+)
+
+
+# Iterate over all configurations
+for robot in "${robots[@]}"; do
+    for env in "${envs[@]}"; do
+        for config_override in "${config_overrides[@]}"; do
+            echo "Running experiment with Robot: $robot, Env: $env, Config Override: $config_override"
+            
+            # Run the Python script with the current configuration
+            python toddlerbot/locomotion/train_mtjx.py --robot "$robot" --env "$env" --config-override "$config_override" --gpu "0" #--restore "$restore"
+            
+            # Optional: Add a small delay between experiments
+            sleep 1
+        done
+    done
+done
+
+
+# 올라프 나중에 학습해야함
+echo "Olaf Study !!!!"
+#python toddlerbot/locomotion/train_mtjx.py --robot "toddlerbot" --env "_T_WalkOlaf" --config-override "PPOConfig.num_timesteps=500000000,PPOConfig.num_evals=1000,PPOConfig.seed=0,TJXEnvConfig.use_basic_obs=False" --gpu "0" #--restore "$restore"
+python toddlerbot/locomotion/train_mtjx.py --robot "toddlerbot" --env "_T_WalkOlaf" --config-override "PPOConfig.num_timesteps=500000000,PPOConfig.num_evals=1000,PPOConfig.seed=0,TJXEnvConfig.use_basic_obs=False,TJXEnvConfig.threshold_ratio=0.3" --gpu "0" #--restore "$restore"
